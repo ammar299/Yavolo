d3fb2e7159204f9a82115701b60e91ca88907993
Ahmad <muhammad.ahmad@phaedrasolutions.com>
2021-05-06 16:16:49 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth' }
+
+  resource :user, only: :update
+  get :profile, to: 'users#show'
+  scope :profile do
+    get :edit, to: 'users#edit', as: :profile_edit
+    get 'password/edit', to: 'users#edit_password', as: :edit_password
+    patch 'password/update', to: 'users#update_password', as: :update_password
+  end
 end
