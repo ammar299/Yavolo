274c4aca08ab73c357211ed619d526b8d8d6c6cb
Ahmad <muhammad.ahmad@phaedrasolutions.com>
2021-05-06 16:14:22 +0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-    #before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :surname, :email_confirmation, :terms_and_conditions, :receive_best_deals])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :surname])
+  end
 end
