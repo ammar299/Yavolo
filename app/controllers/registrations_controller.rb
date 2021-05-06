a2de459aa0f7320710c314c008ab9fa4460180c4
Ahmad <muhammad.ahmad@phaedrasolutions.com>
2021-05-06 19:57:00 +0500

    removed debugger

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,6 @@ class RegistrationsController < Devise::RegistrationsController
     protected
 
         def after_sign_up_path_for(resource)
-            debugger
             if resource.is_a?(User)
                 if resource.seller?
                     seller_dashboard_path
