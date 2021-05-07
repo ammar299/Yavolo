66bfc3714b673f2637bc15cb89daa63fbd287b53
Ahmad <muhammad.ahmad@phaedrasolutions.com>
2021-05-07 12:49:14 +0500

    made changes for social-login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, ENV['facebook_client_id'], ENV['facebook_client_secret'], scope: 'public_profile,email'
+  config.omniauth :google_oauth2, ENV['google_client_id'], ENV['google_secret_id'], scope: 'userinfo.email,userinfo.profile'
+  
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
