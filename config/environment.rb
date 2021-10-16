3f2ec524088eb2d60192ba3a2b53a684e3aafe5e
Talha Waseem <talha.waseem@phaedrasolutions.com>
2021-10-16 02:23:05 -0700

    devise instances settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'gmail.com',
-  authentication: 'plain',
-  enable_starttls_auto: true,
-  user_name: ENV['GMAIL_EMAIL'],
-  password: ENV['GMAIL_PWORD']
+  address: 'smtp-relay.sendinblue.com',
+  port: 587, 
+  domain: 'sendinblue.com',
+  authentication: 'login', 
+  enable_starttls_auto: true, 
+  user_name: 'talha.waseem@phaedrasolutions.com',
+  password: 't13pSOXn0WQcVmrT'
 }
