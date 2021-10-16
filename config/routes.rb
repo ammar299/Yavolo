ba60c656716af4757ee3072ccd7f6ebc2e237995
khawaraslam <khawar.aslam@phaedrasolutions.com>
2021-10-16 15:15:09 +0500

    show alert on filter groups deletion

--- config/routes.rb
+++ config/routes.rb
@@ -69,7 +69,8 @@ Rails.application.routes.draw do
 
         resources :filter_groups do
           collection do
-            delete 'destroy_multiple'
+            get    :confirm_multiple_deletion
+            delete :delete_filter_groups
           end
           member do
             get :confirm_delete
