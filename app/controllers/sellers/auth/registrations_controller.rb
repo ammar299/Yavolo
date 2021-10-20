a7e6007620ad4a019fccf78e093409c65a0c3d16
usama-phaedra <usama.akram@phaedrasolutions.com>
2021-10-20 14:22:44 +0500

    :sparkles: made step step sign up for seller

--- app/controllers/sellers/auth/registrations_controller.rb
+++ app/controllers/sellers/auth/registrations_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Sellers::Auth::RegistrationsController < Devise::RegistrationsController
+  def after_sign_up_path_for(resource)
+    super(resource)
+    sellers_auth_sign_up_steps_path # or whatever path you want here
+  end
 end
--- app/controllers/sellers/dashboard_controller.rb
+++ app/controllers/sellers/dashboard_controller.rb
@@ -1,3 +1,17 @@
 class Sellers::DashboardController < Sellers::BaseController
+    before_action :check_if_seller_profile_completed
     def index; end
+
+    def check_if_seller_profile_completed
+        @seller = authenticate_seller!
+        if @seller.company_detail.nil?
+          redirect_to sellers_auth_sign_up_steps_path
+        elsif @seller.addresses.where(address_type: 'business_address').first.nil?
+          redirect_to sellers_auth_sign_up_steps_path
+        elsif @seller.company_detail.nil?
+          redirect_to sellers_auth_sign_up_steps_path
+        elsif @seller.addresses.where(address_type: 'business_representative_address').first.nil?
+          redirect_to sellers_auth_sign_up_steps_path
+        end
+      end
 end
