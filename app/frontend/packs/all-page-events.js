973473c1f34f270dbbae67aecef77da0fc869f59
usama-phaedra <87006589+usama-phaedra@users.noreply.github.com>
2021-10-28 16:06:25 +0500

    :bug: :hammer: fixed copy to clickup (#106)

--- app/frontend/packs/all-page-events.js
+++ app/frontend/packs/all-page-events.js
@@ -1,6 +1,28 @@
-$(document).ready(function(){
-  $('.notice-cross-icon').click(function(){
-    $('.flash-toast').removeClass('notice-msg');
-    $('.flash-toast').removeClass('alert-msg');
+$(document).ready(function () {
+  $(".notice-cross-icon").click(function () {
+    $(".flash-toast").removeClass("notice-msg");
+    $(".flash-toast").removeClass("alert-msg");
+  });
+  $("body").on("click", ".view-api-token", function () {
+    $(this).next().removeClass("d-none");
+    $(this).hide();
+  });
+
+  $("body").on("click", ".seller-api-token", function () {
+    var text = $(this).text().trim();
+    copyToClipboard(text);
+  });
+
+  $(".api-token").hover(function () {
+    $(".help-popup-text").text("Click to Copy");
   });
 });
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val(element).select();
+  document.execCommand("copy");
+  $temp.remove();
+  $(".help-popup-text").text("copied");
+}
