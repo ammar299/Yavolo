60a5e496ecd1725866339625a9a869060978bf70
khawaraslam <85872744+khawaraslam@users.noreply.github.com>
2021-10-28 13:01:34 +0500

    listing linking filters in dropdown (#101)

--- app/helpers/admin/categories_helper.rb
+++ app/helpers/admin/categories_helper.rb
@@ -1,9 +1,13 @@
 module Admin::CategoriesHelper
   def nested_categories(categories)
-      categories.map do |category, sub_categories|
-        content_tag(:ul, class: "#{category.root == category ? 'active':nil}") do
-          content_tag(:li, ((render 'category', category: category) + nested_categories(sub_categories)).html_safe)
-        end
-      end.join.html_safe
+    categories.map do |category, sub_categories|
+      content_tag(:ul, class: "#{category.root == category ? 'active':nil}") do
+        content_tag(:li, ((render 'category', category: category) + nested_categories(sub_categories)).html_safe)
+      end
+    end.join.html_safe
+  end
+
+  def category_linking_filters(category_id)
+    FilterInCategory.where(filter_group_id: FilterGroup.local_filter_groups.includes(:filter_categories).where(filter_categories: { category_id: category_id })).pluck(:filter_name, :id)
   end
 end
