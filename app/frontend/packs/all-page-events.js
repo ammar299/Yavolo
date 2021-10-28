107f1657dcf763aac4f21961ed28949cbdf7dd97
Talha Waseem <talha.waseem@phaedrasolutions.com>
2021-10-28 05:11:02 -0700

    css

--- app/frontend/packs/all-page-events.js
+++ app/frontend/packs/all-page-events.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
     $('.flash-toast').removeClass('notice-msg');
     $('.flash-toast').removeClass('alert-msg');
   });
+
+  $('body').on('click', '.menu-icon', function(){
+    $('.leftside').toggleClass('leftside-open');
+  });
 });
