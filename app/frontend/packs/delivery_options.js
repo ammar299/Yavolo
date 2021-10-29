969ee030b9ff21e930ba3d0197313cdffc4a5d96
Syed Shan e Raza <85935646+syedshaneraza@users.noreply.github.com>
2021-10-29 17:23:10 +0500

    Delivery options form errors validation (#114)
    * Some Bugs Removed
    
    * delivery options form erros validated
    
    * delivery options form erros validated

--- app/frontend/packs/delivery_options.js
+++ app/frontend/packs/delivery_options.js
@@ -1,9 +1,83 @@
 $(document).ready(function(){
   deleteDeliveryOptionsCarriers();
   deleteDeliveryOptionsForSellers();
+  deliveryOptionDropdownValidation();
+  addNewCarrierFormValidation();
   $('.delivery-options-select-all-container #delivery-option-select-all').change(() => selectedDeliveryOptionsCarriers('.delivery-options-checkbox-container'));
   $('.carriers-select-all-container #carrier-select-all').change(() => selectedDeliveryOptionsCarriers('.carriers-checkbox-container'));
+
 });
+
+function addNewCarrierFormValidation() {
+
+  $('form#add_new_carrier_form').validate({
+    ignore: "", 
+    rules: {
+      "carrier[name]": {
+        required: true
+      },
+      "carrier[api_key]": {
+        required: true
+      },
+      "carrier[secret_key]": {
+        required: true
+      }
+    }, 
+    highlight: function(element) {
+      $(element).parents("div.form-group").addClass('error-field');
+    },
+    unhighlight: function(element) {
+      $(element).parents("div.form-group").removeClass('error-field');
+    },
+    messages: {
+      "carrier[name]": {
+          required: "Name is required"
+      },
+      "carrier[api_key]": {
+        required: "API Key is required"
+      },
+      "carrier[secret_key]": {
+        required: "Secret Key is required"
+      }
+    }
+  });
+
+}
+
+window.setFormValidation = function(){
+  
+  $('form#delivery_option_form').validate({
+    ignore: "", 
+    rules: {
+      "delivery_option[name]": {
+        required: true
+      },
+      "delivery_option[processing_time]": {
+        required: true
+      },
+      "delivery_option[delivery_time]": {
+        required: true
+      }
+    }, 
+    highlight: function(element) {
+      $(element).parents("div.form-group").addClass('error-field');
+    },
+    unhighlight: function(element) {
+      $(element).parents("div.form-group").removeClass('error-field');
+    },
+    messages: {
+      "delivery_option[name]": {
+          required: "Delivery Option is required"
+      },
+      "delivery_option[processing_time]": {
+        required: "Processing Time is required"
+      },
+      "delivery_option[delivery_time]": {
+        required: "Delivery Time is required"
+      }
+    }
+  });
+}
 
 function deleteDeliveryOptionsCarriers(){
   $('body').on('click', '.delete-delivery-options, .delete-carriers', function(){
@@ -42,4 +116,20 @@ function deleteDeliveryOptionsForSellers(){
       type: 'DELETE'
     });
   });
+}
+
+function deliveryOptionDropdownValidation() {
+	$('body').on('change', '#delivery_option_processing_time, #delivery_option_delivery_time', function () {
+    labelId = $(this).is("#delivery_option_processing_time")? '#delivery_option_processing_time-error' : '#delivery_option_delivery_time-error'
+    if ($(this).val() != '') {
+      $(this).parents("div.form-group").removeClass('error-field');
+      $(labelId).text('');
+      $(labelId).css('display', 'd-none');
+    } else {
+      $(this).parents("div.form-group").addClass('error-field');
+      labeltext = $(this).is("#delivery_option_processing_time")? 'Processing Time is Required' : 'Delivery Time is Required'
+      $(labelId).text(labeltext);
+      $(labelId).css('display', 'block');
+    }
+	});
 }
