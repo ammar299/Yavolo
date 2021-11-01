38e8d9555a06dd6cb300cfd7ba3c5e528701fad5
Talha Waseem <talha.waseem@phaedrasolutions.com>
2021-11-01 00:55:40 -0700

    style fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-  config.action_mailer.default_url_options = { :host => "18.132.240.111" }
+  config.action_mailer.default_url_options = { :host => "18.170.174.141" }
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
