41d4e3e0bb5f6c9af5858b5db90e94c1b02e9212
ammarrana299 <82507914+ammarrana299@users.noreply.github.com>
2021-11-18 19:08:05 +0500

    seller will be logout if not perform any action according to timeout … (#246)
    * seller will be logout if not perform any action according to timeout session
    
    * in devlopment rb path changed

--- app/controllers/sellers/base_controller.rb
+++ app/controllers/sellers/base_controller.rb
@@ -1,4 +1,22 @@
 class Sellers::BaseController < ApplicationController
   before_action :authenticate_seller!
   layout 'sellers/seller'
+  before_action :set_last_seen_at, if: proc { seller_signed_in? }
+  before_action :seller_session_expire, if: proc { seller_signed_in? }
+  private
+
+  def set_last_seen_at
+    current_seller.update_attribute(:last_seen_at, Time.current)
+  end
+
+  def seller_session_expire
+    if current_seller.timeout.present?
+      seller_time = Time.at(Time.current - current_seller.last_seen_at).utc.strftime("%M").to_i
+      seller_timeout = Seller.timeouts[current_seller.timeout] * 60
+      if seller_timeout <= seller_time
+        sign_out(current_seller)
+        redirect_to(:controller => 'sellers/auth/sessions', :action => 'new')
+      end
+    end
+  end
 end
