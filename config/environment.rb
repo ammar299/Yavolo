c98086bbd7b1e8f42255c9ff65a493ea7a8aad0f
arhumalihaider <70745268+arhumalihaider@users.noreply.github.com>
2021-11-26 19:41:53 +0500

    email changed (#298)

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,25 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp-relay.sendinblue.com',
+#   port: 587, 
+#   domain: 'sendinblue.com',
+#   authentication: 'login', 
+#   enable_starttls_auto: true, 
+#   user_name: 'talha.waseem@phaedrasolutions.com',
+#   password: 't13pSOXn0WQcVmrT'
+# }
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp-relay.sendinblue.com',
-  port: 587, 
-  domain: 'sendinblue.com',
-  authentication: 'login', 
-  enable_starttls_auto: true, 
-  user_name: 'talha.waseem@phaedrasolutions.com',
-  password: 't13pSOXn0WQcVmrT'
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            "yavolotalha@gmail.com",
+  password:             "talhayavolo123",
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  openssl_verify_mode: 'none' 
 }
+
+# yavolotalha@gmail.com
+  # talhayavolo123
