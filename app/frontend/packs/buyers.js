e25d95729364294447a6030528382f2ccbc7f9f2
Syed Shan e Raza <85935646+syedshaneraza@users.noreply.github.com>
2021-12-21 20:53:38 +0500

    December end fixes (#484)
    * Results-updated + updation-message-updated
    
    * updation-message-updated-again
    
    * background removed + admin-orders-bulk-checkbox
    
    * products-quantity-updated

--- app/frontend/packs/buyers.js
+++ app/frontend/packs/buyers.js
@@ -7,4 +7,28 @@ $(document).ready(function () {
         $(".update-product-quantity").val(quantity);
     });
 
+    $(".update-product-quantity").on('input', function () {
+        changeQtyofInput();
+    });
+
+    $('body').on('click', '.click-product-quantity', function(){
+        $(".update-product-quantity").trigger('change');
+        changeQtyofInput();
+    });
+
+    function changeQtyofInput(){
+        if($("#product_quantity").val() < 1 || $("#product_quantity").val().length == 0)
+        {
+            if($("#quantity-error").length == 0)
+            {
+                $("#buy_button, #add_basket_button").attr("disabled", true);
+                $("#dropdownMenuButton").after('<label  id= "quantity-error" class="text-left w-100 error text-">Please select quantity</label>');
+            }
+        }
+        else
+        {
+            $("#quantity-error").remove();
+            $("#buy_button, #add_basket_button").attr("disabled", false);
+        }
+    }
 });
