4f04ead6dbb549b95784f99f2ceccf9a6ea939d2
syedshaneraza <syed.shaneraza@phaedrasolutions.com>
2021-12-22 19:45:32 +0500

    errors-refixed

--- app/controllers/buyers/products_controller.rb
+++ app/controllers/buyers/products_controller.rb
@@ -4,12 +4,13 @@ class Buyers::ProductsController < ApplicationController
 
   def show
     @product = Product.find_by_handle(params[:id])
+
     unless @product.status == 'active'
       flash.now[:notice] = I18n.t('flash_messages.product_status_disable')
     end
     if params[:preview_listing]
       terms_and_returns = @product.owner_type == "Seller" ? @product.owner&.return_and_term&.instructions : ""
-      @product = session[:preview_listing][:product].merge("terms_and_returns" => terms_and_returns)
+      @product = session[:preview_listing][:product].merge("terms_and_returns" => terms_and_returns, "product_status" => @product.status)
     end
   end
 end
