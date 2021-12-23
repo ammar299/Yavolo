2e78341552b58ba1d87ca14c9a35e6a4a870de22
Syed Shan e Raza <85935646+syedshaneraza@users.noreply.github.com>
2021-12-23 13:36:34 +0500

    Product checkout details (#496)
    * errors-refixed
    
    * preview-listing-errors-resolved

--- app/controllers/buyers/products_controller.rb
+++ app/controllers/buyers/products_controller.rb
@@ -4,13 +4,13 @@ class Buyers::ProductsController < ApplicationController
 
   def show
     @product = Product.find_by_handle(params[:id])
-
-    unless @product.status == 'active'
-      flash.now[:notice] = I18n.t('flash_messages.product_status_disable')
-    end
+    @product_status = @product.status
     if params[:preview_listing]
       terms_and_returns = @product.owner_type == "Seller" ? @product.owner&.return_and_term&.instructions : ""
-      @product = session[:preview_listing][:product].merge("terms_and_returns" => terms_and_returns, "product_status" => @product.status)
+      @product = session[:preview_listing][:product].merge("terms_and_returns" => terms_and_returns)
+    end
+    if @product_status != 'active' && !params[:preview_listing]
+      flash.now[:notice] = I18n.t('flash_messages.product_status_disable')
     end
   end
 end
