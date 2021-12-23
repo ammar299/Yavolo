b53668ba022f199f96f2e1f77d5683c41e1e92a2
ammarrana299 <82507914+ammarrana299@users.noreply.github.com>
2021-12-23 18:33:47 +0500

    Sprint 4 qa fixes (#497)
    * product sample csv changed
    
    * product csv download path changed
    
    * seller import chnages done
    
    * csv seller import issues resolved

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,20 @@ ActiveRecord::Schema.define(version: 2021_12_22_144818) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,6 +74,27 @@ ActiveRecord::Schema.define(version: 2021_12_22_144818) do
     t.string "addressable_type"
     t.index ["addressable_id"], name: "index_addresses_on_addressable_id"
     t.index ["addressable_type"], name: "index_addresses_on_addressable_type"
+  end
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "admins", force: :cascade do |t|
@@ -220,6 +255,15 @@ ActiveRecord::Schema.define(version: 2021_12_22_144818) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "category_linking_filters", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "filter_in_category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_category_linking_filters_on_category_id"
+    t.index ["filter_in_category_id"], name: "index_category_linking_filters_on_filter_in_category_id"
+  end
+
   create_table "company_details", force: :cascade do |t|
     t.string "name"
     t.string "vat_number"
@@ -251,7 +295,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_144818) do
   end
 
   create_table "delivery_option_ships", force: :cascade do |t|
-    t.decimal "price", precision: 8, scale: 2
+    t.float "price"
     t.bigint "delivery_option_id"
     t.bigint "ship_id"
     t.datetime "created_at", precision: 6, null: false
@@ -319,6 +363,22 @@ ActiveRecord::Schema.define(version: 2021_12_22_144818) do
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
+  create_table "google_pay_classes", force: :cascade do |t|
+    t.string "company"
+    t.string "event_name"
+    t.datetime "date"
+    t.string "venue"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "google_pay_event_builders", force: :cascade do |t|
+    t.string "class_id"
+    t.string "company"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "google_shoppings", force: :cascade do |t|
@@ -621,6 +681,11 @@ ActiveRecord::Schema.define(version: 2021_12_22_144818) do
     t.string "recovery_email"
     t.string "otp_secret"
     t.integer "last_otp_at"
+    t.string "encrypted_otp_secret"
+    t.string "encrypted_otp_secret_iv"
+    t.string "encrypted_otp_secret_salt"
+    t.integer "consumed_timestep"
+    t.boolean "otp_required_for_login"
     t.boolean "skip_success_hub_steps", default: false
     t.integer "failed_attempts", default: 0
     t.string "unlock_token"
