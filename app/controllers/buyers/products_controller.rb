b6f18d0696a659c76c9a3ad8395acc1eec90922d
khawaraslam <85872744+khawaraslam@users.noreply.github.com>
2021-12-24 13:02:59 +0500

    quantity limit on carting fixed (#498)

--- app/controllers/buyers/products_controller.rb
+++ app/controllers/buyers/products_controller.rb
@@ -7,7 +7,7 @@ class Buyers::ProductsController < ApplicationController
     @product_status = @product.status
     if params[:preview_listing]
       terms_and_returns = @product.owner_type == "Seller" ? @product.owner&.return_and_term&.instructions : ""
-      @product = session[:preview_listing][:product].merge("terms_and_returns" => terms_and_returns)
+      @product = session[:preview_listing][:product].merge("terms_and_returns" => terms_and_returns, product_stock: @product.stock)
     end
     if @product_status != 'active' && !params[:preview_listing]
       flash.now[:notice] = I18n.t('flash_messages.product_status_disable')
--- app/frontend/packs/buyers.js
+++ app/frontend/packs/buyers.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     console.log('Buyers JS File')
-
+		disableEnterOnAddToCart();
+		disableAddToCartForm();
+    restrictQuantityValue();
     $('.click-product-quantity').click(function (e) {
         e.preventDefault();
         const quantity = $(this).data("quantity");
@@ -17,18 +19,53 @@ $(document).ready(function () {
     });
 
     function changeQtyofInput(){
-        if($("#product_quantity").val() < 1 || $("#product_quantity").val().length == 0)
-        {
-            if($("#quantity-error").length == 0)
-            {
-                $("#buy_button, #add_basket_button").attr("disabled", true);
-                $("#dropdownMenuButton").after('<label  id= "quantity-error" class="text-left w-100 error text-">Please select quantity</label>');
-            }
-        }
-        else
-        {
-            $("#quantity-error").remove();
-            $("#buy_button, #add_basket_button").attr("disabled", false);
-        }
-    }
+			let stock = $('#product_quantity').data('quantity');
+      let qty_val = $("#product_quantity").val();
+			if(qty_val == 0 || qty_val > stock) {
+        $("#quantity-error").remove();
+				if($("#quantity-error").length == 0) {
+					$("#buy_button, #add_basket_button").attr("disabled", true);
+					if (qty_val > stock) {
+						$("#dropdownMenuButton").after('<label  id= "quantity-error" class="text-left w-100 error text-">Available stock is ' + stock +'</label>');
+					}else {
+						$("#dropdownMenuButton").after('<label  id= "quantity-error" class="text-left w-100 error text-">Please select quantity</label>');
+					}
+				}
+			}
+			else {
+				$("#quantity-error").remove();
+				$("#buy_button, #add_basket_button").attr("disabled", false);
+			}
+		}
 });
+
+function disableEnterOnAddToCart() {
+	$('#buyer-products-quantity').on('keyup keypress', function(e) {
+    var keyCode = e.keyCode || e.which;
+    if (keyCode === 13) {
+      e.preventDefault();
+    }
+  });
+}
+
+function disableAddToCartForm() {
+	$('#buy_button, #add_basket_button').click(function (e) {
+		let stock = $('#product_quantity').data('quantity');
+    if($("#product_quantity").val() == 0 || $("#product_quantity").val() > stock) {
+      e.preventDefault();
+    }
+  });
+}
+
+function restrictQuantityValue() {
+  $('.update-product-quantity').keypress(function(e) {
+    var verified = (e.which == 8 || e.which == undefined || e.which == 0) ? null : String.fromCharCode(e.which).match(/[^0-9]/);
+    if (verified || e.delegateTarget.value.length > 3 || e.ctrlKey ==true) {
+      if(e.which!=8 ) {
+        e.preventDefault();
+      }
+    }
+  }).on('paste',function(e) {
+    e.preventDefault();
+  });
+}
