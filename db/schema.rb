be5fa3fa4409cceb437e31465e675b8aa59050da
Syed Shan e Raza <85935646+syedshaneraza@users.noreply.github.com>
2022-01-03 14:21:34 +0500

    Product checkout details (#565)
    * style-issues
    
    * style-issues-again
    
    * text-style-changes
    
    * read-only changed to disabled
    
    * style-issues-by-ahsan
    
    * hover-removed
    
    * hover-removed
    
    * order-summary-block dynamically handled
    
    * style + text issues by ahsan
    
    * typo-error removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_113243) do
+ActiveRecord::Schema.define(version: 2021_12_29_090635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.bigint "resource_id"
-    t.string "author_type"
-    t.bigint "author_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
-  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -74,27 +60,6 @@ ActiveRecord::Schema.define(version: 2021_12_30_113243) do
     t.string "addressable_type"
     t.index ["addressable_id"], name: "index_addresses_on_addressable_id"
     t.index ["addressable_type"], name: "index_addresses_on_addressable_type"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "admins", force: :cascade do |t|
@@ -258,15 +223,6 @@ ActiveRecord::Schema.define(version: 2021_12_30_113243) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "category_linking_filters", force: :cascade do |t|
-    t.bigint "category_id"
-    t.bigint "filter_in_category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_category_linking_filters_on_category_id"
-    t.index ["filter_in_category_id"], name: "index_category_linking_filters_on_filter_in_category_id"
-  end
-
   create_table "company_details", force: :cascade do |t|
     t.string "name"
     t.string "vat_number"
@@ -282,8 +238,6 @@ ActiveRecord::Schema.define(version: 2021_12_30_113243) do
     t.string "amazon_url", default: ""
     t.string "ebay_url", default: ""
     t.string "doing_business_as", default: ""
-    t.string "first_name"
-    t.string "last_name"
     t.index ["seller_id"], name: "index_company_details_on_seller_id"
   end
 
@@ -300,7 +254,7 @@ ActiveRecord::Schema.define(version: 2021_12_30_113243) do
   end
 
   create_table "delivery_option_ships", force: :cascade do |t|
-    t.float "price"
+    t.decimal "price", precision: 8, scale: 2
     t.bigint "delivery_option_id"
     t.bigint "ship_id"
     t.datetime "created_at", precision: 6, null: false
@@ -368,22 +322,6 @@ ActiveRecord::Schema.define(version: 2021_12_30_113243) do
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
-  end
-
-  create_table "google_pay_classes", force: :cascade do |t|
-    t.string "company"
-    t.string "event_name"
-    t.datetime "date"
-    t.string "venue"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "google_pay_event_builders", force: :cascade do |t|
-    t.string "class_id"
-    t.string "company"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "google_shoppings", force: :cascade do |t|
