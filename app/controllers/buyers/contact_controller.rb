e5ddd3a6f673d65a3b3070364edcc00e83f3fd44
Syed Shan e Raza <85935646+syedshaneraza@users.noreply.github.com>
2022-01-06 16:31:14 +0500

    Product checkout details (#597)
    * style-issues
    
    * style-issues-again
    
    * text-style-changes
    
    * read-only changed to disabled
    
    * style-issues-by-ahsan
    
    * hover-removed
    
    * hover-removed
    
    * order-summary-block dynamically handled
    
    * style + text issues by ahsan
    
    * typo-error removed
    
    * contact-us page added + style changes by ahsan
    
    * contact-us message ammended
    
    * conmment removed
    
    * Vaidation added + functionality-verified
    
    * Contact Us Page Complete
    
    * Company name added on review_order_page
    
    * Logo upload button text modified
    
    * Sprint 6 issues resolved
    
    * issues resolved for singular/plural
    
    * address mapping complete
    
    * space added on order page
    
    * contact-us-page successfully submitted
    
    * comment resolved

--- app/controllers/buyers/contact_controller.rb
+++ app/controllers/buyers/contact_controller.rb
@@ -8,13 +8,11 @@ class Buyers::ContactController < Buyers::BaseController
     if params["g-recaptcha-response"].present?
       ContactUsMailer.with(email: params[:email_field], name: params[:name_field], subject: params[:subject_field], msg: params[:your_message_field]).send_contact_form_mail_to_admin.deliver_now
       ContactUsMailer.with(email: params[:email_field], name: params[:name_field], subject: params[:subject_field], msg: params[:your_message_field]).send_email_received_message_to_sender.deliver_now
-      flash[:notice] = "Your response has been recorded"
-      redirect_to contact_us_path
     else
-      @errors = 'ReCaptcha is not filled.'
-      respond_to do |format|  
-        format.js { render 'buyers/contact/contact_us.js.erb'}
-      end
+      @errors = 'Please fill ReCaptcha.'
+    end
+    respond_to do |format|
+      format.js { render 'buyers/contact/contact_us.js.erb'}
     end
   end
 
