1967ef71dbb1146a10f2a98afa48c327a582827e
usama-phaedra <87006589+usama-phaedra@users.noreply.github.com>
2022-01-06 16:31:33 +0500

    :bug: :hammer: fixed facebook login (#598)

--- app/controllers/buyers/auth/omniauth_controller.rb
+++ app/controllers/buyers/auth/omniauth_controller.rb
@@ -2,10 +2,10 @@ class Buyers::Auth::OmniauthController < Devise::OmniauthCallbacksController
 
   # google callback
   def google_oauth2
-    @seller = Buyer.create_from_provider_data(request.env['omniauth.auth'])
-    if @seller.persisted?
+    @buyer = Buyer.create_from_provider_data(request.env['omniauth.auth'])
+    if @buyer.persisted?
       flash[:notice] = 'Signed In Successfully!'
-      sign_in_and_redirect @seller, :event => :authentication #this will throw if @seller is not activated
+      sign_in_and_redirect @buyer, :event => :authentication #this will throw if @buyer is not activated
     else
       session['devise.google_oauth2_data'] = request.env['omniauth.auth']
       redirect_to new_buyer_registration_url
@@ -14,10 +14,10 @@ class Buyers::Auth::OmniauthController < Devise::OmniauthCallbacksController
 
   # facebook callback
   def facebook
-    @seller = Buyer.create_from_provider_data(request.env['omniauth.auth'])
-    if @seller.persisted?
+    @buyer = Buyer.create_from_provider_data(request.env['omniauth.auth'])
+    if @buyer.persisted?
       flash[:notice] = 'Signed In Successfully!'
-      sign_in_and_redirect @seller, :event => :authentication #this will throw if @seller is not activated
+      sign_in_and_redirect @buyer, :event => :authentication #this will throw if @buyer is not activated
     else
       session['devise.facebook_data'] = request.env['omniauth.auth']
       redirect_to new_buyer_registration_url
